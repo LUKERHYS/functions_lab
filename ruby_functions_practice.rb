fde0d4ce8c9cd0fb0f1caca976b8cdf43d8ba0b2
lukerhys <hello@lukemelvin.com>
2020-01-14 19:50:21 +0000

    require 'time' changed to a global variable so both short and long date names can use the one require

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,12 +30,13 @@ def add_string_as_number(str_1, str_2)
   return str_1.to_i() + str_2.to_i()
 end
 
+require 'time'
 def number_to_full_month_name(chosen_month)
-  require 'time'
+  #require 'time'
   return Date::MONTHNAMES[chosen_month]
 end
 
 def number_to_short_month_name(chosen_month)
-  require 'time'
+  #require 'time'
   return Date::ABBR_MONTHNAMES[chosen_month]
 end
