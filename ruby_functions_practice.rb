0d3edb83e2aa18afa75398d80118d678227af060
lukerhys <hello@lukemelvin.com>
2020-01-14 19:48:41 +0000

    number_to_short_month_name and number_to_full_month_name functions added and completed. require 'time' added to each of the functions

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -27,5 +27,15 @@ def join_string(input_1, input_2)
 end
 
 def add_string_as_number(str_1, str_2)
-  return str_1.to_i() + str_2.to_i() 
+  return str_1.to_i() + str_2.to_i()
+end
+
+def number_to_full_month_name(chosen_month)
+  require 'time'
+  return Date::MONTHNAMES[chosen_month]
+end
+
+def number_to_short_month_name(chosen_month)
+  require 'time'
+  return Date::ABBR_MONTHNAMES[chosen_month]
 end
